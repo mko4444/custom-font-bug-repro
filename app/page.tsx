56ce8bfdc3d6d421f772d148d98e48735cf6ea14
matthew <mattk4@me.com>
2024-08-08 20:13:23 -0700

    try logging

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,14 @@ import type { Metadata } from "next";
 export const revalidate = 0;
 
 export async function generateMetadata(): Promise<Metadata> {
+  console.log(
+    "fetching frame data for...",
+    `${process.env.PROD_URL || "http://localhost:3000"}/api`
+  );
   const frameTags = await getFrameMetadata(
     `${process.env.PROD_URL || "http://localhost:3000"}/api`
   );
+  console.log("frame data:", frameTags);
   return {
     other: frameTags,
   };
